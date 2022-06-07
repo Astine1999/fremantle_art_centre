e8c365e2d9fdbc2470e7609d9734d8c200033b4c
Astine1999 <aspoestertjie3@gmail.com>
2022-06-07 18:26:30 +0800

    Create footer.php
    created footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+ <footer>
+          <a href="https://www.facebook.com/fremantleartscentre" class="fa fa-facebook"></a>
+<a href="https://twitter.com/FreoArtsCentre" class="fa fa-twitter"></a>
+          <a href="https://www.instagram.com/fremantleartscentre/" class="fa fa-instagram"></a>
+          <p class="foot text-centre">©Fremantle Arts Centre 2022</p>
+        </footer>
+        <?php wp_footer(); ?>
+</body>
+</html>
