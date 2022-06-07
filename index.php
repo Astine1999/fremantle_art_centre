158fffbdfc1a95cab3f6fbc492d76af7b26434c2
Astine1999 <aspoestertjie3@gmail.com>
2022-06-07 18:26:48 +0800

    Create index.php
    created index.php

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php get_header();?>   <!--  Tells WordPress to include header.php -->
+      <section class= "container-fluid  text-center">
+        <div class="container bg-3">
+<h2 class="title2">Welcome!</h2>
+        </div> <!-- container -->
+            <div class="row">
+              <div class="col-md-6">
+<p class="info">Welcome to the Fremantle Art Centre (FAC). We are situated at Walyalup on Whadjuk Nyoongar Boodjar. 
+  We are here to bring the community of Perth vibrant and rich entertainment in the form
+  of music, events and markets. All of these locally sourced around suburbs in Perth. FAC’s exhibitions program presents 
+  and commissions work by artists practicing locally, nationally and globally, offering a diverse selection of contemporary art. 
+  All FAC exhibitions are free to visit to ensure our visual arts program is accessible and inclusive of everyone.
+</p> 
+              </div> <!-- col -->
+          <div class="col-md-6 img1">
+            <img src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/img1.png"class="img-fluid1" alt="dancing">
+          </div>
+        </div> <!-- row -->
+    </section>
+
+    <section class= "container-fluid bg-4 text-center">
+      <div class="container">
+<h2 class="title2-5">What we offer</h2>
+<div class="row " >
+  <div class="col-md-4">
+    <img src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/music.png" class="img-fluid" alt="music">
+    <h5> Music</h5>
+    <p class="pinfo">Music for all your needs and desires!!</p>
+  </div> <!-- col -->
+  <div class="col-md-4">
+    <img src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/market.png" class="img-fluid" alt="food"> 
+    <h5> Market</h5>
+    <p class="pinfo">If you need food and drinks, we have you covered.</p>
+  </div> <!-- col -->
+  <div class="col-md-4">
+    <img src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/art.png" class="img-fluid" alt="paint">
+    <h5> Art</h5>
+    <p class="pinfo">We have a wide variety of art by local artists up for display</p>
+  </div> <!-- col -->
+</div> <!-- row -->
+      </div> <!-- container -->
+      <a href="blog.html" class="btn btn-info btn-lg" role="button">Blog</a>
+      </section>
+
+      <section>
+        <div class="container-fluid sign-up">
+          <div class="container">
+            <div class="row">
+
+              <div class="col-md-6">
+                <h2 class="tag">The largest <br>community of Music lovers yet!</h2>
+                </div>
+
+              <div class="col-md-6">
+      <div class="signupFrm">
+        <form action="" class="form">
+          <h2 class="title3">Sign up</h1>
+    
+          <div class="inputContainer">
+            <input type="text" class="input" placeholder="a">
+            <label for="" class="label">Email</label>
+          </div>
+    
+          <div class="inputContainer">
+            <input type="text" class="input" placeholder="a">
+            <label for="" class="label">Username</label>
+          </div>
+    
+          <div class="inputContainer">
+            <input type="text" class="input" placeholder="a">
+            <label for="" class="label">Password</label>
+          </div>
+    
+          <div class="inputContainer">
+            <input type="text" class="input" placeholder="a">
+            <label for="" class="label">Confirm Password</label>
+          </div>
+    
+          <input type="submit" class="submitBtn" value="Sign up">
+        </form>
+      </div>
+        </div>
+      </div>
+    </div>
+      </div>
+        </section>
+        <?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
