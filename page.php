5195f9b38ff34cf86ce1568d852915427c90e766
Astine1999 <aspoestertjie3@gmail.com>
2022-06-10 11:25:16 +0800

    Update page.php

--- page.php
+++ page.php
@@ -1,9 +1,3 @@
-<?php
-/*
-Template Name: Home Page
-*/
-?>
-
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
 <div class="container-fluid maincontainer">
     <div class="container">
