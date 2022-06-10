d14f1a73749f63a28034f2ac282ddfbf08a247cb
Astine1999 <aspoestertjie3@gmail.com>
2022-06-10 11:24:56 +0800

    Update header.php

--- header.php
+++ header.php
@@ -63,7 +63,26 @@
                </li>
         </ul>
       </div>
-    </nav>
+
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+     <?php 
+
+wp_nav_menu( array(
+  'theme_location'  => 'primary',
+  'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+  'container'       => 'div',
+  'container_class' => 'collapse navbar-collapse',
+  'container_id'    => 'bs-example-navbar-collapse-1',
+  'menu_class'      => 'navbar-nav ml-auto',
+  'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+  'walker'          => new WP_Bootstrap_Navwalker(),
+) );
+
+?>
+</nav>
 
   <div class="container-fluid bg-2 text-center">   <!-- div for the main image the stretches to the edge of the page- -->
     <div class="container"><!-- div that holds the content in the middle of the page-->
