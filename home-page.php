36f4b71c22173977efcf7b209740198180ab2405
Astine1999 <aspoestertjie3@gmail.com>
2022-06-12 23:45:30 +0800

    Update home-page.php
    removed middle information

--- home-page.php
+++ home-page.php
@@ -3,48 +3,44 @@
     Template Name: Home page
     */
     ?>
-<?php get_header();?>   <!--  Tells WordPress to include header.php -->
+<?php require_once('header.php'); ?>
       <section class= "container-fluid  text-center">
         <div class="container bg-3">
-<h2 class="title2"><?php the_field('maintagline');?></h2>
+        <h2><?php the_field('taglinetitle'); ?></h2><!--  custom field for the aboutme content-->
         </div> <!-- container -->
             <div class="row">
               <div class="col-md-6">
-<p class="info">Welcome to the Fremantle Art Centre (FAC). We are situated at Walyalup on Whadjuk Nyoongar Boodjar. 
-  We are here to bring the community of Perth vibrant and rich entertainment in the form
-  of music, events and markets. All of these locally sourced around suburbs in Perth. FAC’s exhibitions program presents 
-  and commissions work by artists practicing locally, nationally and globally, offering a diverse selection of contemporary art. 
-  All FAC exhibitions are free to visit to ensure our visual arts program is accessible and inclusive of everyone.
-</p> 
+<p class="info"><?php the_field('welcometext'); ?></p>
+
               </div> <!-- col -->
           <div class="col-md-6 img1">
-            <img src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/img1.png"class="img-fluid1" alt="dancing">
+          <img class="img-fluid1" alt=dancing src="<?php the_field('img1welcome');?>">
           </div>
         </div> <!-- row -->
     </section>
 
     <section class= "container-fluid bg-4 text-center">
       <div class="container">
-<h2 class="title2-5">What we offer</h2>
+<h2 class="title2-5"><?php the_field('offertagline');?></h2>
 <div class="row " >
   <div class="col-md-4">
-    <img src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/music.png" class="img-fluid" alt="music">
-    <h5> Music</h5>
-    <p class="pinfo">Music for all your needs and desires!!</p>
+    <img class="img-fluid" alt="music" src="<?php the_field('offerimg1');?>">
+    <h5><?php the_field('offersubtag1');?></h5>
+    <p class="pinfo"><?php the_field('offerpara1');?></p>
   </div> <!-- col -->
   <div class="col-md-4">
-    <img src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/market.png" class="img-fluid" alt="food"> 
-    <h5> Market</h5>
-    <p class="pinfo">If you need food and drinks, we have you covered.</p>
+    <img class="img-fluid" alt="food" src="<?php the_field('offerimg2');?>"> 
+    <h5><?php the_field('offersubtag2');?></h5>
+    <p class="pinfo"><?php the_field('offerpara2');?></p>
   </div> <!-- col -->
   <div class="col-md-4">
-    <img src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/art.png" class="img-fluid" alt="paint">
-    <h5> Art</h5>
-    <p class="pinfo">We have a wide variety of art by local artists up for display</p>
+    <img class="img-fluid" alt="paint" src="<?php the_field('offerimg3');?>">
+    <h5><?php the_field('offersubtag3');?></h5>
+    <p class="pinfo"><?php the_field('offerpara3');?></p>
   </div> <!-- col -->
 </div> <!-- row -->
       </div> <!-- container -->
-      <a href="blog.html" class="btn btn-info btn-lg" role="button">Blog</a>
+      <a href="<?php the_field('offerbutton');?>" class="btn btn-info btn-lg" role="button">Blog</a>
       </section>
 
       <section>
