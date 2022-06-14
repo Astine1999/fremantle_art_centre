e84ac1e1c1a70d34e7eeb55c9f52ccf59e4ebb52
Astine1999 <aspoestertjie3@gmail.com>
2022-06-15 00:06:44 +0800

    Create info.php

---
+++ info.php
@@ -0,0 +1,71 @@
+<?php get_header();?>   <!--  Tells WordPress to include header.php -->
+      <section class= "container-fluid text-center">
+        <div class="container bg-7">
+<h2 class="title2">What is is?</h2>
+        </div> <!-- container -->
+            <div class="row">
+              <div class="col-md-6">
+<p class="info3">The fremantle arts festival presents Flanked by deep waters, 
+    our state and our nation’s connection with the coast is complex and multilayered. 
+    From narratives of the beginning of time, of creation, to our collective imaginings 
+    of the future, the oceans that surround our island continent and the oceans and seas 
+    of our global community are sites of tradition, transformation, transience, terror, 
+    and turmoil. This local band writes and performs songs to represent the necessity to 
+    maintain and look after our oceans.
+</p> 
+              </div> <!-- col -->
+          <div class="col-md-6 img2">
+            <img src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/img2.png" alt="group of band members" class="img-responsive1">
+          </div>
+        </div> <!-- row -->
+    </section>
+
+    <section class="container-fluid bg-8">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img  src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/img3.png" alt="twinklylightswithpeople" class="img-responsive3">
+                </div>
+                <!--col-->
+                <div class="col-sm-6 info4">
+                    <h2> Artists</h2>
+                    <ul>
+                   <li>Amanda Bell (Yued & Badimia People, WA)</li> 
+                   <li>Sam Bloor (WA)</li> 
+                    <li>Ron Bradfield (Bard People, WA)</li>
+                    <li>Elisa Jane Carmichael (Quandamooka People, QLD)</li>
+                    <li>Sonja Carmichael (Quandamooka People, QLD)</li>
+                    <li>Garry Sibosado (WA)</li>
+                    <li>Soul Alphabet (WA)</li>
+                    <li>Angela Tiatia (Samoan/Australian, NSW)</li>
+                </ul>
+                </div>
+                <!--col-->
+            </div>
+            <!--row-->
+        </div>
+        <!--container-->
+    </section>
+
+    <section class="container-fluid maps text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                <h2 class="maps-title"> Map</h2>
+                    <p class="maps-address">1 Finnerty St, Fremantle WA 6160</p>
+                    <img  src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/map1.png" alt="map of FAC" class="img-map img-responsive">
+                </div>
+                <!--col-->
+                <div class="col-sm-6">
+                <h2 class="parking-title"> Parking available</h2>
+                    <p class="parking-address">CPP Fremantle</p>
+                    <img  src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/map2.png" alt="map of CPP parking" class="img-map img-responsive">
+                </div>
+                <!--col-->
+            </div>
+            <!--row-->
+        </div>
+        <!--container-->
+        <a href="#" class="btn2 btn-info btn-lg" role="button">Book now</a>
+    </section>
+    <?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
