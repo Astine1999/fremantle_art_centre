4d9c12a70e39136cb1af9a97f815ea117fd658b0
Astine1999 <aspoestertjie3@gmail.com>
2022-06-16 08:36:37 +0800

    Update footer.php
    edited php

--- footer.php
+++ footer.php
@@ -1,8 +1,7 @@
  <footer>
-          <a href="https://www.facebook.com/fremantleartscentre" class="fa fa-facebook"></a>
-<a href="https://twitter.com/FreoArtsCentre" class="fa fa-twitter"></a>
-          <a href="https://www.instagram.com/fremantleartscentre/" class="fa fa-instagram"></a>
-          <p class="foot text-centre">©Fremantle Arts Centre 2022</p>
+ <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
+
         </footer>
         <?php wp_footer(); ?>
 </body>
