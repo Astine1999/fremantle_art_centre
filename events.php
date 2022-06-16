484cff45faac99000942a2ede4277a4a8585a9e5
Astine1999 <aspoestertjie3@gmail.com>
2022-06-16 08:36:25 +0800

    Update events.php

--- events.php
+++ events.php
@@ -5,6 +5,12 @@
           <p class="events-subpara text-center"> This Week</p>
         </div>
       </section>
+ <section class="container-fluid events">
+        <div class="container">
+          <h2 class="events-title text-center"> Events</h2>
+          <p class="events-subpara text-center"> This Week</p>
+        </div>
+      </section>
 
       <section class="events-imgs-container-fluid">
           <div class="container">
@@ -12,14 +18,14 @@
 
                   <div class=col-sm-3>
                       <div class="row">
-                          <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e1.png">
+                          <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e1.png" alt="abba">
                           <div class=col-9>
                               <a class="event-title" href="info.html">ABBA Returns</a>
                               <p class="event-time">4.30pm-6.30pm</p>
                           </div>
   
                           <div class=col-3>
-                              <p class="date"><b>01</b></p>
+                              <p class="date1"><strong>01</strong></p>
                               <p class="month">April</p>
                           </div>
                       </div> <!-- row -->
@@ -27,14 +33,14 @@
   
                   <div class=col-sm-3>
                       <div class="row">
-                          <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e2.png">
+                          <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e2.png" alt="INSC">
                           <div class=col-9>
-                              <h2 class="event-title">INSC</h2>
+                              <a class="event-title">INSC</a>
                               <p class="event-time">3.30pm-9.30pm</p>
                           </div>
 
                           <div class=col-3>
-                            <p class="date"><b>03</b></p>
+                            <p class="date2"><strong>03</strong></p>
                             <p class="month">April</p>
                           </div>
                       </div> <!-- row -->
@@ -42,14 +48,14 @@
 
                   <div class=col-sm-3>
                       <div class="row">
-                          <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e3.png">
+                          <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e3.png" alt="80's night">
                           <div class=col-9>
-                              <h2 class="event-title">80's Night</h2>
+                              <a class="event-title">80's Night</a>
                               <p class="event-time">12:00pm-2:00pm</p>
                           </div>
 
                           <div class=col-3>
-                            <p class="date"><b>05</b></p>
+                            <p class="date3"><strong>05</strong></p>
                             <p class="month">April</p>
                           </div>
                       </div> <!-- row -->
@@ -57,13 +63,13 @@
 
                   <div class=col-sm-3>
                       <div class="row">
-                          <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e4.png">
+                          <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e4.png" alt="undertow">
                           <div class=col-9>
-                              <h2 class="event-title">Under<br>tow</h2>
+                              <a class="event-title">Under<br>tow</a>
                               <p class="event-time">9:00am-11:00am</p>
                           </div>
                           <div class=col-3>
-                            <p class="date"> <b>06</b></p>
+                            <p class="date4"> <strong>06</strong></p>
                             <p class="month">April</p>
                           </div>
                       </div> <!-- row -->
@@ -79,14 +85,14 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e5.png">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e5.png" alt="fanta kits">
                         <div class=col-9>
-                            <h2 class="event-title">Fanta Kits</h2>
+                            <a class="event-title">Fanta Kits</a>
                             <p class="event-time">2:30pm-4:00pm</p>
                         </div>
 
                         <div class=col-3>
-                            <p class="date"><b>07</b></p>
+                            <p class="date5"><strong>07</strong></p>
                             <p class="month">April</p>
                         </div>
                     </div> <!-- row -->
@@ -94,14 +100,14 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e6.png">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e6.png" alt="friday">
                         <div class=col-9>
-                            <h2 class="event-title">Friday Story</h2>
+                            <a class="event-title">Friday Story</a>
                             <p class="event-time">5:00pm-7:30pm</p>
                         </div>
 
                         <div class=col-3>
-                          <p class="date"><b>08</b></p>
+                          <p class="date6"><strong>08</strong></p>
                           <p class="month">April</p>
                         </div>
                     </div> <!-- row -->
@@ -109,14 +115,14 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e7.png">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e7.png" alt="mosh pit">
                         <div class=col-9>
-                            <h2 class="event-title">Mosh Live</h2>
+                            <a class="event-title">Mosh Live</a>
                             <p class="event-time">7:00pm-9:00pm</p>
                         </div>
 
                         <div class=col-3>
-                          <p class="date"><b>09</b></p>
+                          <p class="date7"><strong>09</strong></p>
                           <p class="month">April</p>
                         </div>
                     </div> <!-- row -->
@@ -124,13 +130,13 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e8.png">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e8.png" alt="acdc">
                         <div class=col-9>
-                            <h2 class="event-title">ACDC</h2>
+                            <a class="event-title">ACDC</a>
                             <p class="event-time">8:00pm-12:00am</p>
                         </div>
                         <div class=col-3>
-                          <p class="date"> <b>10</b></p>
+                          <p class="date8"> <strong>10</strong></p>
                           <p class="month">April</p>
                         </div>
                     </div> <!-- row -->
@@ -152,14 +158,14 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e9.png">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e9.png" alt="xa xiu">
                         <div class=col-9>
-                            <h2 class="event-title">Xa Xiu</h2>
+                            <a class="event-title">Xa Xiu</a>
                             <p class="event-time">1:00pm-7:00pm</p>
                         </div>
 
                         <div class=col-3>
-                            <p class="date"><b>11</b></p>
+                            <p class="date9"><strong>11</strong></p>
                             <p class="month">April</p>
                         </div>
                     </div> <!-- row -->
@@ -167,15 +173,14 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e10.png">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e10.png" alt="farmers market">
                         <div class=col-9>
-                            <h2 class="event-title">Farmers Live</h2>
+                            <a class="event-title">Farmers Live</a>
                             <p class="event-time">4.30pm- 6.30pm</p>
-                            <a href="info.html" class="explore">Explore....</a>
                         </div>
 
                         <div class=col-3>
-                          <p class="date"><b>13</b></p>
+                          <p class="date10"><strong>13</strong></p>
                           <p class="month">April</p>
                         </div>
                     </div> <!-- row -->
@@ -183,15 +188,14 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e11.png">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e11.png" alt="manny">
                         <div class=col-9>
-                            <h2 class="event-title">Manny Event</h2>
+                            <a class="event-title">Manny Event</a>
                             <p class="event-time">5:00pm-7:00pm</p>
-                            <a href="info.html" class="explore">Explore....</a>
                         </div>
 
                         <div class=col-3>
-                          <p class="date"><b>14</b></p>
+                          <p class="date11"><strong>14</strong></p>
                           <p class="month">April</p>
                         </div>
                     </div> <!-- row -->
@@ -199,14 +203,13 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e12.png">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e12.png" alt="ruby">
                         <div class=col-9>
-                            <h2 class="event-title">Ruby Parade</h2>
+                            <a class="event-title">Ruby Parade</a>
                             <p class="event-time">6:30pm-7:30pm</p>
-                            <a href="info.html" class="explore">Explore....</a>
                         </div>
                         <div class=col-3>
-                          <p class="date"> <b>15</b></p>
+                          <p class="date12"> <strong>15</strong></p>
                           <p class="month">April</p>
                         </div>
                     </div> <!-- row -->
@@ -222,15 +225,14 @@
 
               <div class=col-sm-3>
                   <div class="row">
-                      <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e13.png">
+                      <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e13.png" alt="live piano">
                       <div class=col-9>
-                          <h2 class="event-title">Live Piano</h2>
+                          <a class="event-title">Live Piano</a>
                           <p class="event-time">8:30pm-11:30pm</p>
-                          <a href="info.html" class="explore">Explore....</a>
                       </div>
 
                       <div class=col-3>
-                          <p class="date"><b>16</b></p>
+                          <p class="date13"><strong>17</strong>></p>
                           <p class="month">April</p>
                       </div>
                   </div> <!-- row -->
@@ -238,15 +240,14 @@
 
               <div class=col-sm-3>
                   <div class="row">
-                      <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e14.png">
+                      <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e14.png" alt="stories">
                       <div class=col-9>
-                          <h2 class="event-title">Story Time</h2>
+                          <a class="event-title">Story Time</a>
                           <p class="event-time">10:00am-2:30pm</p>
-                          <a href="info.html" class="explore">Explore....</a>
                       </div>
 
                       <div class=col-3>
-                        <p class="date"><b>18</b></p>
+                        <p class="date14"><strong>18</strong></p>
                         <p class="month">April</p>
                       </div>
                   </div> <!-- row -->
@@ -254,15 +255,14 @@
 
               <div class=col-sm-3>
                   <div class="row">
-                      <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e15.png">
+                      <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e15.png" alt="insc">
                       <div class=col-9>
-                          <h2 class="event-title">INSC</h2>
+                          <a class="event-title">INSC</a>
                           <p class="event-time">7:00am-9:00am</p>
-                          <a href="info.html" class="explore2">Explore....</a>
                       </div>
 
                       <div class=col-3>
-                        <p class="date"><b>19</b></p>
+                        <p class="date15"><strong>19</strong>></p>
                         <p class="month">April</p>
                       </div>
                   </div> <!-- row -->
@@ -270,14 +270,13 @@
 
               <div class=col-sm-3>
                   <div class="row">
-                      <img class="img-fluid" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e16.png">
+                      <img class="img-responsive" src="http://206.189.45.97/~mesh22/sub1/wp-content/themes/Fremantle_art_centre/fremantle_art_centre/images/e16.png" alt="dmas music">
                       <div class=col-9>
-                          <h2 class="event-title">DMAS Music</h2>
+                          <a class="event-title">DMAS Music</a>
                           <p class="event-time">11:45am-3:30pm</p>
-                          <a href="info.html" class="explore">Explore....</a>
                       </div>
                       <div class=col-3>
-                        <p class="date"> <b>20</b></p>
+                        <p class="date16"> <strong>20</strong></p>
                         <p class="month">April</p>
                       </div>
                   </div> <!-- row -->
