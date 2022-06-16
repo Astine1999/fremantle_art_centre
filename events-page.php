5443ef56ef2e358de759d9083d6070af09ac5616
Astine1999 <aspoestertjie3@gmail.com>
2022-06-16 08:36:28 +0800

    Update events-page.php

--- events-page.php
+++ events-page.php
@@ -17,14 +17,14 @@
 
                   <div class=col-sm-3>
                       <div class="row">
-                          <img class="img-responsive" src="<?php the_field('sectionimg1');?>">
+                          <img class="img-responsive" src="<?php the_field('sectionimg1');?>" alt="abba">
                           <div class=col-9>
                               <a class="event-title" href="<?php the_field('title1');?>">ABBA Returns</a>
                               <p class="event-time"><?php the_field('sectiontime1');?></p>
                           </div>
   
                           <div class=col-3>
-                              <p class="date1"><b><?php the_field('sectiondate1');?></b></p>
+                              <p class="date1"><strong><?php the_field('sectiondate1');?></strong></p>
                               <p class="month"><?php the_field('sectionmonth1');?></p>
                           </div>
                       </div> <!-- row -->
@@ -32,14 +32,14 @@
   
                   <div class=col-sm-3>
                       <div class="row">
-                          <img class="img-responsive" src="<?php the_field('sectionimg2');?>">
+                          <img class="img-responsive" src="<?php the_field('sectionimg2');?>" alt="insc">
                           <div class=col-9>
                               <a class="event-title" href="<?php the_field('title2');?>">INSC</a>
                               <p class="event-time"><?php the_field('sectiontime2');?></p>
                           </div>
 
                           <div class=col-3>
-                            <p class="date2"><b><?php the_field('sectiondate2');?></b></p>
+                            <p class="date2"><strong><?php the_field('sectiondate2');?></strong></p>
                             <p class="month"><?php the_field('sectionmonth2');?></p>
                           </div>
                       </div> <!-- row -->
@@ -47,14 +47,14 @@
 
                   <div class=col-sm-3>
                       <div class="row">
-                          <img class="img-responsive" src="<?php the_field('sectionimg3');?>">
+                          <img class="img-responsive" src="<?php the_field('sectionimg3');?>"  alt="80'snight">
                           <div class=col-9>
                               <a class="event-title" href="<?php the_field('title3');?>">80's Night</a>
                               <p class="event-time"><?php the_field('sectiontime3');?></p>
                           </div>
 
                           <div class=col-3>
-                            <p class="date3"><b><?php the_field('sectiondate3');?></b></p>
+                            <p class="date3"><strong><?php the_field('sectiondate3');?></strong></p>
                             <p class="month"><?php the_field('sectionmonth3');?></p>
                           </div>
                       </div> <!-- row -->
@@ -62,13 +62,13 @@
 
                   <div class=col-sm-3>
                       <div class="row">
-                          <img class="img-responsive" src="<?php the_field('sectionimg4');?>">
+                          <img class="img-responsive" src="<?php the_field('sectionimg4');?>"  alt="undertow">
                           <div class=col-9>
                               <a class="event-title" href="<?php the_field('title4');?>">Under<br>tow</a>
                               <p class="event-time"><?php the_field('sectiontime4');?></p>
                           </div>
                           <div class=col-3>
-                            <p class="date4"> <b><?php the_field('sectiondate4');?></b></p>
+                            <p class="date4"> <strong><?php the_field('sectiondate4');?></strong></p>
                             <p class="month"><?php the_field('sectionmonth4');?></p>
                           </div>
                       </div> <!-- row -->
@@ -84,14 +84,14 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-responsive" src="<?php the_field('sectionimg5');?>">
+                        <img class="img-responsive" src="<?php the_field('sectionimg5');?>"  alt="fanta">
                         <div class=col-9>
                             <a class="event-title" href="<?php the_field('title5');?>">Fanta Kits</a>
                             <p class="event-time"><?php the_field('sectiontime5');?></p>
                         </div>
 
                         <div class=col-3>
-                            <p class="date5"><b><?php the_field('sectiondate5');?></b></p>
+                            <p class="date5"><strong><?php the_field('sectiondate5');?></strong></p>
                             <p class="month"><?php the_field('sectionmonth5');?></p>
                         </div>
                     </div> <!-- row -->
@@ -99,14 +99,14 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-responsive" src="<?php the_field('sectionimg6');?>">
+                        <img class="img-responsive" src="<?php the_field('sectionimg6');?>"  alt="friday">
                         <div class=col-9>
                             <a class="event-title" href="<?php the_field('title6');?>">Friday Story</a>
                             <p class="event-time"><?php the_field('sectiontime6');?></p>
                         </div>
 
                         <div class=col-3>
-                          <p class="date6"><b><?php the_field('sectiondate6');?></b></p>
+                          <p class="date6"><strong><?php the_field('sectiondate6');?></strong></p>
                           <p class="month"><?php the_field('sectionmonth6');?></p>
                         </div>
                     </div> <!-- row -->
@@ -114,14 +114,14 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-responsive" src="<?php the_field('sectionimg7');?>">
+                        <img class="img-responsive" src="<?php the_field('sectionimg7');?>"  alt="mosh">
                         <div class=col-9>
                             <a class="event-title" href="<?php the_field('title7');?>">Mosh Live</a>
                             <p class="event-time"><?php the_field('sectiontime7');?></p>
                         </div>
 
                         <div class=col-3>
-                          <p class="date7"><b><?php the_field('sectiondate7');?></b></p>
+                          <p class="date7"><strong><?php the_field('sectiondate7');?></strong></p>
                           <p class="month"><?php the_field('sectionmonth7');?></p>
                         </div>
                     </div> <!-- row -->
@@ -129,13 +129,13 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-responsive" src="<?php the_field('sectionimg8');?>">
+                        <img class="img-responsive" src="<?php the_field('sectionimg8');?>"  alt="acdc">
                         <div class=col-9>
                             <a class="event-title" href="<?php the_field('title8');?>">ACDC</a>
                             <p class="event-time"><?php the_field('sectiontime8');?></p>
                         </div>
                         <div class=col-3>
-                          <p class="date8"> <b><?php the_field('sectiondate8');?></b></p>
+                          <p class="date8"> <strong><?php the_field('sectiondate8');?></strong></p>
                           <p class="month"><?php the_field('sectionmonth8');?></p>
                         </div>
                     </div> <!-- row -->
@@ -157,14 +157,14 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-responsive" src="<?php the_field('sectionimg9');?>">
+                        <img class="img-responsive" src="<?php the_field('sectionimg9');?>"  alt="xa xiu">
                         <div class=col-9>
                             <a class="event-title" href="<?php the_field('title9');?>">Xa Xiu</a>
                             <p class="event-time"><?php the_field('sectiontime9');?></p>
                         </div>
 
                         <div class=col-3>
-                            <p class="date9"><b><?php the_field('sectiondate9');?></b></p>
+                            <p class="date9"><strong><?php the_field('sectiondate9');?></strong></p>
                             <p class="month"><?php the_field('sectionmonth9');?></p>
                         </div>
                     </div> <!-- row -->
@@ -172,14 +172,14 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-responsive" src="<?php the_field('sectionimg10');?>">
+                        <img class="img-responsive" src="<?php the_field('sectionimg10');?>"  alt="farmers">
                         <div class=col-9>
                             <a class="event-title" href="<?php the_field('title10');?>">Farmers Live</a>
                             <p class="event-time"><?php the_field('sectiontime10');?></p>
                         </div>
 
                         <div class=col-3>
-                          <p class="date10"><b><?php the_field('sectiondate10');?></b></p>
+                          <p class="date10"><strong><?php the_field('sectiondate10');?></strong></p>
                           <p class="month"><?php the_field('sectionmonth10');?></p>
                         </div>
                     </div> <!-- row -->
@@ -187,14 +187,14 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-responsive" src="<?php the_field('sectionimg11');?>">
+                        <img class="img-responsive" src="<?php the_field('sectionimg11');?>"  alt="manny">
                         <div class=col-9>
                             <a class="event-title" href="<?php the_field('title11');?>">Manny Event</a>
                             <p class="event-time"><?php the_field('sectiontime11');?></p>
                         </div>
 
                         <div class=col-3>
-                          <p class="date11"><b><?php the_field('sectiondate11');?></b></p>
+                          <p class="date11"><strong><?php the_field('sectiondate11');?></strong></p>
                           <p class="month"><?php the_field('sectionmonth11');?></p>
                         </div>
                     </div> <!-- row -->
@@ -202,13 +202,13 @@
 
                 <div class=col-sm-3>
                     <div class="row">
-                        <img class="img-responsive" src="<?php the_field('sectionimg12');?>">
+                        <img class="img-responsive" src="<?php the_field('sectionimg12');?>"  alt="ruby">
                         <div class=col-9>
                             <a class="event-title" href="<?php the_field('title12');?>">Ruby Parade</a>
                             <p class="event-time"><?php the_field('sectiontime12');?></p>
                         </div>
                         <div class=col-3>
-                          <p class="date12"> <b><?php the_field('sectiondate12');?></b></p>
+                          <p class="date12"> <strong><?php the_field('sectiondate12');?></strong></p>
                           <p class="month"><?php the_field('sectionmonth12');?></p>
                         </div>
                     </div> <!-- row -->
@@ -224,14 +224,14 @@
 
               <div class=col-sm-3>
                   <div class="row">
-                      <img class="img-responsive" src="<?php the_field('sectionimg13');?>">
+                      <img class="img-responsive" src="<?php the_field('sectionimg13');?>"  alt="live piano">
                       <div class=col-9>
                           <a class="event-title" href="<?php the_field('title13');?>">Live Piano</a>
                           <p class="event-time"><?php the_field('sectiontime13');?></p>
                       </div>
 
                       <div class=col-3>
-                          <p class="date13"><b><?php the_field('sectiondate13');?></b></p>
+                          <p class="date13"><strong><?php the_field('sectiondate13');?></strong></p>
                           <p class="month"><?php the_field('sectionmonth13');?></p>
                       </div>
                   </div> <!-- row -->
@@ -239,14 +239,14 @@
 
               <div class=col-sm-3>
                   <div class="row">
-                      <img class="img-responsive" src="<?php the_field('sectionimg14');?>">
+                      <img class="img-responsive" src="<?php the_field('sectionimg14');?>"  alt="stories">
                       <div class=col-9>
                           <a class="event-title" href="<?php the_field('title14');?>">Story Time</a>
                           <p class="event-time"><?php the_field('sectiontime14');?></p>
                       </div>
 
                       <div class=col-3>
-                        <p class="date14"><b><?php the_field('sectiondate14');?></b></p>
+                        <p class="date14"><strong><?php the_field('sectiondate14');?></strong></p>
                         <p class="month"><?php the_field('sectionmonth14');?></p>
                       </div>
                   </div> <!-- row -->
@@ -254,14 +254,14 @@
 
               <div class=col-sm-3>
                   <div class="row">
-                      <img class="img-responsive" src="<?php the_field('sectionimg15');?>">
+                      <img class="img-responsive" src="<?php the_field('sectionimg15');?>"  alt="insc">
                       <div class=col-9>
                           <a class="event-title" href="<?php the_field('title15');?>">INSC</a>
                           <p class="event-time"><?php the_field('sectiontime15');?></p>
                       </div>
 
                       <div class=col-3>
-                        <p class="date15"><b><?php the_field('sectiondate15');?></b></p>
+                        <p class="date15"><strong><?php the_field('sectiondate15');?></strong></p>
                         <p class="month"><?php the_field('sectionmonth15');?></p>
                       </div>
                   </div> <!-- row -->
@@ -269,13 +269,13 @@
 
               <div class=col-sm-3>
                   <div class="row">
-                      <img class="img-responsive" src="<?php the_field('sectionimg16');?>">
+                      <img class="img-responsive" src="<?php the_field('sectionimg16');?>"  alt="dmas">
                       <div class=col-9>
                           <a class="event-title" href="<?php the_field('title16');?>">DMAS Music</a>
                           <p class="event-time"><?php the_field('sectiontime16');?></p>
                       </div>
                       <div class=col-3>
-                        <p class="date16"> <b><?php the_field('sectiondate16');?></b></p>
+                        <p class="date16"> <strong><?php the_field('sectiondate16');?></strong></p>
                         <p class="month"><?php the_field('sectionmonth16');?></p>
                       </div>
                   </div> <!-- row -->
